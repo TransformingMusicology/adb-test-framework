adeac8850aa0c67e148e2b1608dc72d97500e616
Richard Lewis <richard.lewis@gold.ac.uk>
2016-02-25 15:38:06 +0000

    Started writing precision tests; implementations of 1.0 precision

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -64,8 +64,132 @@ testRankingCmp = TestInstance {
                                  then Pass
                                  else Fail $ "Compare rankings not correct: r1 == r2: " ++ (show (r1 == r2)) ++ "; r2 /= r1: " ++ (show (r2 /= r1))
 
+dummyQueryOpts :: QueryOpts
+dummyQueryOpts = QueryOpts {
+    qo_type              = SequenceQuery
+  , qo_key               = "Track1"
+  , qo_start             = 0.0
+  , qo_length            = 1.0
+  , qo_pointNN           = 0
+  , qo_radius            = 0.0
+  , qo_resultLen         = 0
+  , qo_sequenceHop       = 0.0
+  , qo_absoluteThreshold = Nothing
+  , qo_relativeThreshold = Nothing
+  , qo_unitNorming       = True
+  , qo_distance          = EuclideanNormed
+  , qo_rotations         = Nothing }
+
+dummyDB :: Database
+dummyDB = Database {
+    db_fileName    = ""
+  , db_feature     = ""
+  , db_frameSize   = fromIntegral
+  , db_featureRate = ceiling
+  , db_power       = True }
+
+track1_start0_dist0 :: Ranking
+track1_start0_dist0 = Ranking {
+    rk_key          = "Track1"
+  , rk_distance     = 0.0
+  , rk_distThresh   = Nothing
+  , rk_start        = 0.0
+  , rk_startThresh  = Nothing
+  , rk_length       = Just 1.0
+  , rk_lengthThresh = Nothing }
+
+track1_start1_dist0 :: Ranking
+track1_start1_dist0 = Ranking {
+    rk_key          = "Track1"
+  , rk_distance     = 0.0
+  , rk_distThresh   = Nothing
+  , rk_start        = 1.0
+  , rk_startThresh  = Nothing
+  , rk_length       = Just 1.0
+  , rk_lengthThresh = Nothing }
+
+track1_start2_dist01 :: Ranking
+track1_start2_dist01 = Ranking {
+    rk_key          = "Track1"
+  , rk_distance     = 0.1
+  , rk_distThresh   = Nothing
+  , rk_start        = 1.0
+  , rk_startThresh  = Nothing
+  , rk_length       = Just 1.0
+  , rk_lengthThresh = Nothing }
+
+track1_start3_dist05 :: Ranking
+track1_start3_dist05 = Ranking {
+    rk_key          = "Track1"
+  , rk_distance     = 0.5
+  , rk_distThresh   = Nothing
+  , rk_start        = 3.0
+  , rk_startThresh  = Nothing
+  , rk_length       = Just 1.0
+  , rk_lengthThresh = Nothing }
+
+track1_start4_dist10 :: Ranking
+track1_start4_dist10 = Ranking {
+    rk_key          = "Track1"
+  , rk_distance     = 1.0
+  , rk_distThresh   = Nothing
+  , rk_start        = 5.0
+  , rk_startThresh  = Nothing
+  , rk_length       = Just 1.0
+  , rk_lengthThresh = Nothing }
+
+testMatchDistancesPrecision10 :: TestInstance
+testMatchDistancesPrecision10 = TestInstance {
+    run = return $ checkPrecision
+  , name = "MatchDistances precision 1.0"
+  , tags = []
+  , options = []
+  , setOption = \_ _ -> Right testMatchDistancesPrecision10
+  }
+  where
+    requiredResults = [track1_start0_dist0, track1_start1_dist0, track1_start2_dist01, track1_start3_dist05, track1_start4_dist10]
+    returnedResults = requiredResults
+    requiredPrecision = 1.0
+    (_, computedPrecision, _) = evaluate returnedResults query
+    query = Query { q_identifier      = "TestMatchDistancePrecision10"
+                  , q_db              = dummyDB
+                  , q_query           = dummyQueryOpts
+                  , q_specifiedBy     = "Tester"
+                  , q_requiredResults = requiredResults
+                  , q_evaluation      = MatchDistances }
+    checkPrecision = Finished $
+                       if computedPrecision == requiredPrecision
+                       then Pass
+                       else Fail $ "Precision 1.0 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
+
+testMatchOrderPrecision10 :: TestInstance
+testMatchOrderPrecision10 = TestInstance {
+    run = return $ checkPrecision
+  , name = "MatchOrder precision 1.0"
+  , tags = []
+  , options = []
+  , setOption = \_ _ -> Right testMatchOrderPrecision10
+  }
+  where
+    requiredResults = [track1_start0_dist0, track1_start1_dist0, track1_start2_dist01, track1_start3_dist05, track1_start4_dist10]
+    returnedResults = requiredResults
+    requiredPrecision = 1.0
+    (_, computedPrecision, _) = evaluate returnedResults query
+    query = Query { q_identifier      = "TestMatchDistancePrecision10"
+                  , q_db              = dummyDB
+                  , q_query           = dummyQueryOpts
+                  , q_specifiedBy     = "Tester"
+                  , q_requiredResults = requiredResults
+                  , q_evaluation      = MatchOrder }
+    checkPrecision = Finished $
+                       if computedPrecision == requiredPrecision
+                       then Pass
+                       else Fail $ "Precision 1.0 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
+
 tests :: IO [Test]
 tests = withStderrLogging $
           return [ Test testInMaybeRange
                  , Test testRankingCmp
+                 , Test testMatchDistancesPrecision10
+                 , Test testMatchOrderPrecision10
                  ]
