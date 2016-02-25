132a13be2eee234de680bd28ba3da0499edd5dea
Richard Lewis <richard.lewis@gold.ac.uk>
2016-02-25 16:23:03 +0000

    Added some non-1.0 precision tests

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -186,10 +186,135 @@ testMatchOrderPrecision10 = TestInstance {
                        then Pass
                        else Fail $ "Precision 1.0 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
 
+testMatchDistancesPrecisionAllDifferent :: TestInstance
+testMatchDistancesPrecisionAllDifferent = TestInstance {
+    run = return $ checkPrecision
+  , name = "MatchDistances all different; precision 0.0"
+  , tags = []
+  , options = []
+  , setOption = \_ _ -> Right testMatchDistancesPrecisionAllDifferent
+  }
+  where
+    requiredResults = [track1_start0_dist0, track1_start1_dist0, track1_start2_dist01]
+    returnedResults = [track1_start3_dist05, track1_start4_dist10]
+    requiredPrecision = 0.0
+    (_, computedPrecision, _) = evaluate returnedResults query
+    query = Query { q_identifier      = "TestMatchDistancesPrecisionAllDifferent"
+                  , q_db              = dummyDB
+                  , q_query           = dummyQueryOpts
+                  , q_specifiedBy     = "Tester"
+                  , q_requiredResults = requiredResults
+                  , q_evaluation      = MatchDistances }
+    checkPrecision = Finished $
+                       if computedPrecision == requiredPrecision
+                       then Pass
+                       else Fail $ "Precision 0.0 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
+
+testMatchOrderPrecisionAllMisplaced :: TestInstance
+testMatchOrderPrecisionAllMisplaced = TestInstance {
+    run = return $ checkPrecision
+  , name = "MatchOrder all misplaced; precision 0.0"
+  , tags = []
+  , options = []
+  , setOption = \_ _ -> Right testMatchOrderPrecisionAllMisplaced
+  }
+  where
+    requiredResults = [track1_start0_dist0, track1_start1_dist0, track1_start2_dist01, track1_start3_dist05, track1_start4_dist10]
+    returnedResults = [track1_start1_dist0, track1_start0_dist0, track1_start3_dist05, track1_start4_dist10, track1_start2_dist01]
+    requiredPrecision = 0.0
+    (_, computedPrecision, _) = evaluate returnedResults query
+    query = Query { q_identifier      = "TestMatchOrderPrecisionAllMisplaced"
+                  , q_db              = dummyDB
+                  , q_query           = dummyQueryOpts
+                  , q_specifiedBy     = "Tester"
+                  , q_requiredResults = requiredResults
+                  , q_evaluation      = MatchOrder }
+    checkPrecision = Finished $
+                       if computedPrecision == requiredPrecision
+                       then Pass
+                       else Fail $ "Precision 0.0 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
+
+testMatchDistancesPrecisionOneDifferentHead :: TestInstance
+testMatchDistancesPrecisionOneDifferentHead = TestInstance {
+    run = return $ checkPrecision
+  , name = "MatchDistances one different at head; precision 0.75"
+  , tags = []
+  , options = []
+  , setOption = \_ _ -> Right testMatchDistancesPrecisionOneDifferentHead
+  }
+  where
+    requiredResults = [track1_start1_dist0, track1_start2_dist01, track1_start3_dist05, track1_start4_dist10]
+    returnedResults = [track1_start0_dist0, track1_start2_dist01, track1_start3_dist05, track1_start4_dist10]
+    requiredPrecision = 0.75
+    (_, computedPrecision, _) = evaluate returnedResults query
+    query = Query { q_identifier      = "TestMatchDistancesPrecisionOneDifferentHead"
+                  , q_db              = dummyDB
+                  , q_query           = dummyQueryOpts
+                  , q_specifiedBy     = "Tester"
+                  , q_requiredResults = requiredResults
+                  , q_evaluation      = MatchDistances }
+    checkPrecision = Finished $
+                       if computedPrecision == requiredPrecision
+                       then Pass
+                       else Fail $ "Precision 0.75 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
+
+testMatchDistancesPrecisionOneDifferentLast :: TestInstance
+testMatchDistancesPrecisionOneDifferentLast = TestInstance {
+    run = return $ checkPrecision
+  , name = "MatchDistances one different at end; precision 0.75"
+  , tags = []
+  , options = []
+  , setOption = \_ _ -> Right testMatchDistancesPrecisionOneDifferentLast
+  }
+  where
+    requiredResults = [track1_start0_dist0, track1_start1_dist0, track1_start2_dist01, track1_start3_dist05]
+    returnedResults = [track1_start0_dist0, track1_start1_dist0, track1_start2_dist01, track1_start4_dist10]
+    requiredPrecision = 0.75
+    (_, computedPrecision, _) = evaluate returnedResults query
+    query = Query { q_identifier      = "TestMatchDistancesPrecisionOneDifferentLast"
+                  , q_db              = dummyDB
+                  , q_query           = dummyQueryOpts
+                  , q_specifiedBy     = "Tester"
+                  , q_requiredResults = requiredResults
+                  , q_evaluation      = MatchDistances }
+    checkPrecision = Finished $
+                       if computedPrecision == requiredPrecision
+                       then Pass
+                       else Fail $ "Precision 0.75 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
+
+testMatchOrderPrecisionTwoTransposed :: TestInstance
+testMatchOrderPrecisionTwoTransposed = TestInstance {
+    run = return $ checkPrecision
+  , name = "MatchOrder two transposed; precision 0.6"
+  , tags = []
+  , options = []
+  , setOption = \_ _ -> Right testMatchOrderPrecisionTwoTransposed
+  }
+  where
+    requiredResults = [track1_start0_dist0, track1_start1_dist0, track1_start2_dist01, track1_start3_dist05, track1_start4_dist10]
+    returnedResults = [track1_start1_dist0, track1_start0_dist0, track1_start2_dist01, track1_start3_dist05, track1_start4_dist10]
+    requiredPrecision = 0.6
+    (_, computedPrecision, _) = evaluate returnedResults query
+    query = Query { q_identifier      = "TestMatchOrderPrecisionTwoTransposed"
+                  , q_db              = dummyDB
+                  , q_query           = dummyQueryOpts
+                  , q_specifiedBy     = "Tester"
+                  , q_requiredResults = requiredResults
+                  , q_evaluation      = MatchOrder }
+    checkPrecision = Finished $
+                       if computedPrecision == requiredPrecision
+                       then Pass
+                       else Fail $ "Precision 0.6 mismatch: expected: " ++ (show requiredPrecision) ++ "; computed: " ++ (show computedPrecision)
+
 tests :: IO [Test]
 tests = withStderrLogging $
           return [ Test testInMaybeRange
                  , Test testRankingCmp
                  , Test testMatchDistancesPrecision10
                  , Test testMatchOrderPrecision10
+                 , Test testMatchDistancesPrecisionAllDifferent
+                 , Test testMatchOrderPrecisionAllMisplaced
+                 , Test testMatchDistancesPrecisionOneDifferentHead
+                 , Test testMatchDistancesPrecisionOneDifferentLast
+                 , Test testMatchOrderPrecisionTwoTransposed
                  ]
